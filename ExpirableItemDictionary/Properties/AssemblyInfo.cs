85178389c66e6a108ab1d9a328c8a4bb98ea266e
Stefan Rado <git@sradonia.net>
2013-11-28 15:04:35 +0100

    Set AssemblyInfo attributes to some meaningful values.

--- ExpirableItemDictionary/Properties/AssemblyInfo.cs
+++ ExpirableItemDictionary/Properties/AssemblyInfo.cs
@@ -1,26 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExpirableDictionary")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ExpirableDictionary")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("efc8aa5c-0f1e-4eb6-a4d0-f5e00eea6831")]
+[assembly: AssemblyTitle("ExpirableItemDictionary")]
+[assembly: AssemblyDescription("Expirable Item Dictionary")]
+[assembly: AssemblyProduct("ExpirableItemDictionary")]
+[assembly: AssemblyCopyright("Copyright © 2010 Jon Davis")]
 
 // Version information for an assembly consists of the following four values:
 //
--- ExpirableItemDictionaryTests/Properties/AssemblyInfo.cs
+++ ExpirableItemDictionaryTests/Properties/AssemblyInfo.cs
@@ -1,26 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ExpirableDictionaryTests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("ExpirableDictionaryTests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("a7d4e122-7060-4de3-82a0-cc903b6415db")]
+[assembly: AssemblyTitle("ExpirableItemDictionaryTests")]
+[assembly: AssemblyDescription("Tests for the Expirable Item Dictionary")]
+[assembly: AssemblyProduct("ExpirableItemDictionary")]
+[assembly: AssemblyCopyright("Copyright © 2010 Jon Davis")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +17,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
